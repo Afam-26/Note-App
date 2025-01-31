8ed92aa08b3c77b2bbec8cf88e56f759bcfceba5
Afamefuna Anekeh <afamefuna.anekeh@optimizely.com>
2025-01-31 14:20:52 -0600

    pushing schema & routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,38 @@ const express = require ('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile)
+  async function(accessToken, refreshToken, profile, done) {
+    
+    const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value
+    }
+
+    try {
+
+        let user = await User.findOne( { googleId: profile.id});
+
+        if(user) {
+            done(null, user);
+        } else {
+            user = await User.create(newUser);
+            done(null, user);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
   }
 ));
 
@@ -33,5 +57,28 @@ router.get('/login-failure', (req, res)=> {
     res.send('something went wrong...')}
 )
 
+// Destroy user session
+router.get('logout', (req, res) => {
+    req.session.destroy(err => {
+        if(err) {
+            console.log(err);
+            res.send('Error loggin out');
+        } else {
+            res.redirect('/')
+        }
+    })
+});
+
+// Persist user data after successful auth
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+})
+
+// Retrieve user data from session
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    })
+})
 
 module.exports = router;
